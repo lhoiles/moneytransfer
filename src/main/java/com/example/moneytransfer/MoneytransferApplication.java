2c869a486e96c27089d8b7b1cee42b30300a65e4
Laura Hoiles <laurahoiles639@gmail.com>
2025-07-03 16:37:11 +0200

    Fix component scanning to include all controller and service packages for working API and tests

--- src/main/java/com/example/moneytransfer/MoneytransferApplication.java
+++ src/main/java/com/example/moneytransfer/MoneytransferApplication.java
@@ -3,7 +3,7 @@ package com.example.moneytransfer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.moneytransfer", "com.moneytransfer"})
 public class MoneytransferApplication {
 
 	public static void main(String[] args) {
